6bd00919f375a855710be721de51b5401e8d7f34
jajohn3102 <114176693+jajohn3102@users.noreply.github.com>
2023-02-22 11:18:14 -0500

    finalized dummy user class according to naming conventions

--- skeleton_project/lib/user_class.dart
+++ skeleton_project/lib/user_class.dart
@@ -1,39 +1,29 @@
 class User {
   // Instance variables
-  String? username;
-  String? password;
-  String? email;
+  String username;
+  String password;
+  String email;
 
   // Constructor
-  User(String this.username, String this.password, String this.email);
+  User(this.username, this.password, this.email);
 
-  void setUsername(String username) {
-    this.username = username;
-  }
+  // Mutators
+  set setUsername(String username) => this.username = username;
 
-  void setPassword(String password) {
-    this.password = password;
-  }
+  set setPassword(String password) => this.password = password;
 
-  void setEmail(String email) {
-    this.email = email;
-  }
+  set setEmail(String email) => this.email = email;
 
-  String? getUsername() {
-    return username;
-  }
+  // Accessors
+  String get getUsername => username;
 
-  String? getPassword() {
-    return password;
-  }
+  String get getPassword => password;
 
-  String? getEmail() {
-    return email;
-  }
+  String get getEmail => email;
 }
-
+// Sample Data
 User user1 = User("Jaj7271", "Jaj72777", "Jaj7271@uncw.edu");
 User user2 = User("djd1234", "djd72247", "fds1032@uncw.edu");
 User user3 = User("tjs2224", "tjd9222", "tjs2234@uncw.edu");
 
-var sample_users = [user1, user2, user3];
+var sampleUsers = [user1, user2, user3];
