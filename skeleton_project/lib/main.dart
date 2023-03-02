7e5a27eae6e915271e1cbcc53c36e108589681e1
j-daggs <johnadaggs@gmail.com>
2023-03-02 12:15:15 -0500

    Implemented a Comment Page, contains a floating button that opens a Modal Bottom Sheet containing instances of Comment as Strings. Will later be merged with Event Page so that it contains a Comment Section.

--- skeleton_project/lib/main.dart
+++ skeleton_project/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'comment.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -24,7 +24,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const CommentPage(title: 'Comment Page'), // for quick switch to run and test comment page
     );
   }
 }
@@ -112,4 +113,89 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+// Page to hold comment section, comment section will be integrated into Event Page
+class CommentPage extends StatefulWidget{
+  const CommentPage ({super.key, required this.title});
+  final String title;
+  @override
+  State<CommentPage> createState() => _MyCommentPage();
+}
+
+class _MyCommentPage extends State<CommentPage>{
+  var commentList = Comment.testingList;
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'This Page implements a Modal Bottom Sheet for a Comment Section. Press the floating button to view.',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => showModalBottomSheet(
+          context: context, 
+          builder: (context) => buildSheet(),
+          ),
+        child: const Icon(Icons.comment_rounded), // add comment icon to floating button
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+  
+  /// build for Modal Bottom Sheet Comment section
+  Widget buildSheet() => Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    // comments added for testing 
+    children: [
+      Text(
+        commentList[0].toString(),
+      ),
+      Text(
+        commentList[1].toString(),
+      ),
+      Text(
+        commentList[2].toString(),
+      ),
+      Text(
+        commentList[3].toString(),
+      ),
+      Text(
+        commentList[4].toString(),
+      ),
+    ],
+  );
 }
