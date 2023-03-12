917f5eedc8e9750a1333ecdd92e2124f3be222e8
Nick Buono <nbuono23@gmail.com>
2023-03-12 14:17:20 -0400

    playing around with authentication/database stuff

---
+++ skeleton_project/lib/screens/home/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('home'),
+    );
+  }
+}
