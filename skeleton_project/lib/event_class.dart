5841ebd84cb15b4053ad870bf4dc3249e7a9b402
jbutecht <89483702+jbutecht@users.noreply.github.com>
2023-03-12 19:28:36 -0400

    removed get and set methods, changed starttime, endtime, date to DateTime objects

--- skeleton_project/lib/event_class.dart
+++ skeleton_project/lib/event_class.dart
@@ -4,83 +4,54 @@ import 'dart:js_util';
 class Event {
   String username;
   String sport;
-  int starttime;
-  int endtime;
-  String date;
+  DateTime starttime;
+  DateTime endtime;
+  DateTime date;
   String address;
   String skill;
   String description;
+  List comments;
   int postid;
 
   Event(this.username, this.sport, this.starttime, this.endtime, this.date,
-      this.address, this.skill, this.description, this.postid);
-
-  //getter and setter for username
-  String get getUsername => username;
-  set setUsername(String name) => username = name;
-
-  //getter and setter for sport
-  String get getSport => sport;
-  set setSport(String sportname) => sport = sportname;
-
-  //getter and setter for starttime
-  int get getStartTime => starttime;
-  set setStartTime(int time) => starttime = time;
-
-  //getter and setter for endtime
-  int get getEndTime => endtime;
-  set setEndTime(int time) => endtime = time;
-
-  //getter and setter for date
-  String get getDate => date;
-  set setDate(String eventdate) => date = eventdate;
-
-  //getter and setter for address
-  String get getAddress => address;
-  set setAddress(String eventaddress) => address = eventaddress;
-
-  //getter and setter for skill
-  String get getSkillLevel => skill;
-  set setSkillLevel(String skilllevel) => skill = skilllevel;
-
-  //getter and setter for description
-  String get getDescription => description;
-  set setDescription(String eventdescription) => description = eventdescription;
-
-  //getter and setter for postid
-  int get getPostID => postid;
-  set setPostID(int id) => postid = id;
+      this.address, this.skill, this.description, this.comments, this.postid);
 }
 
+var sampleDate1 = DateTime.utc(2023, 15, 3, 15, 30);
+var sampleDate2 = DateTime.utc(2023, 20, 4, 9, 00);
+var sampleDate3 = DateTime.utc(2023, 18, 2, 17, 15);
 var SAMPLE_EVENTS = [
   Event(
       "user1",
       "Tennis",
-      1330,
-      1530,
-      "03/15/2023",
+      sampleDate1,
+      sampleDate1,
+      sampleDate1,
       "1234 Tennis Dr. Wilmington, NC",
       "Beginner",
       "Looking for another beginner level player to play a singles match.",
+      ['comment1', 'comment2'],
       123),
   Event(
       "user2",
       "Soccer",
-      0900,
-      1200,
-      "04/20/2023",
+      sampleDate2,
+      sampleDate2,
+      sampleDate2,
       "589 Seahawk Dr. Wilimington, NC",
       "Intermediate",
       "5v5 soccer game, already have 7 need 3 more",
+      ['comment3', 'comment4'],
       8756),
   Event(
       "user3",
       "Basketball",
-      1700,
-      2000,
-      "02/28/2023",
+      sampleDate3,
+      sampleDate3,
+      sampleDate3,
       "927 Market St. Wilimington, NC",
       "Expert",
       "4v4 pickup basketball game, I have a team and am looking for another team of 4.",
+      ['comment5', 'comment6'],
       9768)
 ];
