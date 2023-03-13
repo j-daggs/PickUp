e34f0afa86edbb245b43ab25e74d251e03b098e5
jbutecht <89483702+jbutecht@users.noreply.github.com>
2023-03-13 01:39:37 -0400

    change view_event_page structure, incorporated Event class into file

--- skeleton_project/lib/view_event_page.dart
+++ skeleton_project/lib/view_event_page.dart
@@ -1,4 +1,6 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'package:flutter/material.dart';
+import 'event_class.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,84 +9,138 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // #docregion titleSection
-    Widget eventDetails = Container(
-      padding: const EdgeInsets.all(32),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            padding: const EdgeInsets.all(20),
-            child: const Text(
-              'Sport:',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+    return MaterialApp(
+      title: 'View Event',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class EventData {
+  Event event1 = Event(
+      "user1",
+      "Tennis",
+      sampleDate1,
+      sampleDate1,
+      sampleDate1,
+      "1234 Tennis Dr. Wilmington, NC",
+      "Beginner",
+      "Looking for another beginner level player to play a singles match.",
+      ['comment1', 'comment2'],
+      123);
+  Event event2 = Event(
+      "user2",
+      "Soccer",
+      sampleDate2,
+      sampleDate2,
+      sampleDate2,
+      "589 Seahawk Dr. Wilimington, NC",
+      "Intermediate",
+      "5v5 soccer game, already have 7 need 3 more",
+      ['comment3', 'comment4'],
+      8756);
+  Event event3 = Event(
+      "user3",
+      "Basketball",
+      sampleDate3,
+      sampleDate3,
+      sampleDate3,
+      "927 Market St. Wilimington, NC",
+      "Expert",
+      "4v4 pickup basketball game, I have a team and am looking for another team of 4.",
+      ['comment5', 'comment6'],
+      9768);
+}
+
+class MyHomePage extends StatelessWidget {
+  get onPressed => null;
+  var eventSport = EventData().event1.sport;
+  var eventSkill = EventData().event1.skill;
+  var eventLoc = EventData().event1.address;
+  var eventDate = EventData().event1.date;
+  var eventStart = EventData().event1.starttime;
+  var eventEnd = EventData().event1.endtime;
+  var eventDesc = EventData().event1.description;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'Sport: $eventSport',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.left,
+              ),
             ),
           ),
-          Container(
-            padding: const EdgeInsets.all(20),
-            child: const Text(
-              'Skill Level:',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Text('Skill: $eventSkill',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.left),
             ),
           ),
-          Container(
-            padding: const EdgeInsets.all(20),
-            child: const Text(
-              'Location:',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Text('Location: $eventLoc',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
             ),
           ),
-          Container(
-            padding: const EdgeInsets.all(20),
-            child: const Text(
-              'Date:',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Text('Date: $eventDate',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
             ),
           ),
-          Container(
-            padding: const EdgeInsets.all(20),
-            child: const Text(
-              'Time:',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Text('Start Time: $eventStart',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
             ),
           ),
-          Container(
-            padding: const EdgeInsets.all(20),
-            child: const Text(
-              'Description:',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Text('End Time: $eventEnd',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
             ),
           ),
-          Container(
-            padding: const EdgeInsets.all(20),
-            child: const Text(
-              'Comments:',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Text('Description: $eventDesc',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
             ),
           ),
           Align(
             alignment: Alignment.bottomRight,
             child: FloatingActionButton(
-              onPressed: () {},
-              child: const Icon(
-                Icons.star,
-                color: Colors.white,
-              ),
-            ),
+                onPressed: onPressed,
+                tooltip: 'Interested',
+                child: Icon(
+                  Icons.star_border_rounded,
+                  size: 35,
+                )),
           ),
-        ],
-      ),
-    );
-
-    return MaterialApp(
-      title: 'View Event',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Event Details'),
-        ),
-        body: Column(
-          children: [eventDetails],
-        ),
+        ]),
       ),
     );
   }
