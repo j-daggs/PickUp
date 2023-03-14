d5d13a4004a3ad2fb23db2fbc09d8d449feaec5e
jajohn3102 <114176693+jajohn3102@users.noreply.github.com>
2023-03-13 21:56:41 -0400

    Fixed pubspec.yaml duplications and other erros

--- skeleton_project/lib/main.dart
+++ skeleton_project/lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 import 'auth/main_page.dart';
 
 void main() async {
