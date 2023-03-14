2319ca7ceb95478a4151722fa60c3184f12609a9
jbutecht <89483702+jbutecht@users.noreply.github.com>
2023-03-13 21:07:14 -0400

    home_page dart import fix
    fixed import line for event_class in home_page

--- skeleton_project/lib/home_page.dart
+++ skeleton_project/lib/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/event_class.dart';
+import 'package:my_app/classes/event_class.dart';
 
 class HomePage extends StatelessWidget {
   var sampleEvents = Event.getEvent;
