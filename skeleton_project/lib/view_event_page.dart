76c4b877afa3fe6281791689e57aa3ca92d61cfa
jbutecht <89483702+jbutecht@users.noreply.github.com>
2023-03-13 21:04:37 -0400

    import event_class.dart fix
    fixed import lines in home_page and view_event_page to have correct path to import event_class

--- skeleton_project/lib/view_event_page.dart
+++ skeleton_project/lib/view_event_page.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 import 'package:flutter/material.dart';
-import 'event_class.dart';
+import 'package:my_app/classes/event_class.dart';
 import 'package:intl/intl.dart';
 
 void main() => runApp(const MyApp());
