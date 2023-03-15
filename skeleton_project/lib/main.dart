718fa9ce8f481e2d5e2643b2d7d4fee1cb39f9c7
j-daggs <johnadaggs@gmail.com>
2023-03-15 18:15:42 -0400

    added comment section to event page

--- skeleton_project/lib/main.dart
+++ skeleton_project/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'auth/main_page.dart';
+import 'view_event_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
