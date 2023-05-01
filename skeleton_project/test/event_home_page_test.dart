b53c0fedaf48fe56a0a4105806b5782059a37277
evanspn <espn135@gmail.com>
2023-05-01 10:54:03 -0400

    committing test

--- skeleton_project/test/event_home_page_test.dart
+++ skeleton_project/test/event_home_page_test.dart
@@ -1,27 +1,42 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
-import 'package:my_app/classes/event_class.dart';
+import 'package:intl/intl.dart';
 import 'package:my_app/pages/event_home_page.dart';
-import 'package:my_app/pages/view_event_page.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  testWidgets('EventHomePage AppBar displays buttons',
-      (WidgetTester tester) async {
-    // Build the EventHomePage widget
-    await tester.pumpWidget(
-      const MaterialApp(
-        home: HomePage(),
-      ),
-    );
+void main() {
+  testWidgets('Test EventHomePage', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: HomePage()));
+    final sportDropdown = find.text('All Sports');
+    final skillDropdown = find.text('All Skill Levels');
+    // Verify initial state of input menu
+    expect(sportDropdown, findsOneWidget);
+    expect(skillDropdown, findsOneWidget);
 
-    // Verify that the widget shows the 'Add' and 'Filter' buttons in the AppBar
-    expect(find.widgetWithIcon(AppBar, Icons.add), findsOneWidget);
-    expect(find.widgetWithIcon(AppBar, Icons.arrow_drop_down_rounded),
-        findsWidgets);
+    // Open sport dropdown menu
+    await tester.tap(find.descendant(
+        of: find.byType(DropdownButton), matching: sportDropdown));
+    await tester.pumpAndSettle();
+
+    // Select 'Soccer' from sport dropdown menu
+    await tester.tap(find.text('Soccer').last);
+    await tester.pumpAndSettle();
+
+    // Verify sport dropdown menu was updated
+    expect(find.text('Soccer'), findsOneWidget);
+
+    // Open skill level dropdown menu
+    await tester.tap(find.descendant(
+        of: find.byType(DropdownButton), matching: skillDropdown));
+    await tester.pumpAndSettle();
+
+    // Select 'Intermediate' from skill level dropdown menu
+    await tester.tap(find.text('Intermediate').last);
+    await tester.pumpAndSettle();
+
+    // Verify skill level dropdown menu was updated
+    expect(find.text('Intermediate'), findsOneWidget);
+
+    // Verify that an event is found by the address
+    expect(find.text('Nature Park'), findsOneWidget);
   });
 }
